66886715ffd77ecafca54f55df72481cae1fd930
joelleishimwe <joelle.ishimwe@ocsbstudent.ca>
2018-04-19 11:42:43 -0400

    add

--- main.lua
+++ main.lua
@@ -1,7 +1,34 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: CompanyLogo
+-- Name: Joelle Ishimwe
+-- Course: ICS2O
+-- Description: This calls the splash screen of the app to load itself.
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- Hiding Status Bar
+-- HIDE THE STATUS BAR
+
+-----------------------------------------------------------------------------------------
+
+-- Use composer library
+local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+
+-- Go to the intro screen
+composer.gotoScene( "splash_screen" )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
